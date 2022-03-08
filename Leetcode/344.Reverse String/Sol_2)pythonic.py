3b372ef39d0d806de56b6ea1f5431c3866014725
plooox <insukim97@gmail.com>
2022-03-08 18:42:20 +0900

    2022-03-08 Leetcode

---
+++ Leetcode/344.Reverse String/Sol_2)pythonic.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+				# 1. reverse()
+        s.reverse()
+				# 2. Slicing
+				s = s[::-1]  # Do not run in Leetcode: Memory Complexity is not O(1)
+				s[:] = s[::1]
