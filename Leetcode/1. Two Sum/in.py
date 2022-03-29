6cc0dfe8a09c5ed91c99a717da7150de5085cdbd
plooox <insukim97@gmail.com>
2022-03-29 15:57:48 +0900

    Rename:Title 제목 수정
    Clear

--- Leetcode/1. Two Sum/in.py
+++ Leetcode/1. Two Sum/in.py
@@ -4,7 +4,7 @@ class Solution:
         for idx, n in enumerate(nums):
             sub_target = target - n
             
-            if sub_target in nums[idx+1:]:
+            if sub_target in nums[idx+1 : ]:
 								#  nums.index(sub_target)은 중복되는 값 케어 x ex) [3,3]
 								#  --->  nums[idx+1:].index(sub_target) + (idx + 1)
                 return [nums.index(n), nums[idx+1:].index(sub_target) + (idx + 1)]
